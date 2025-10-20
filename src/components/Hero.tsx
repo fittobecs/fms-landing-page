a78357159de27d2085955786d57c17979efa20c7
김종완 <fittobe.cs@gmail.com>
2025-10-20 15:10:23 +0900

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero: React.FC = () => {
   return (
     <section
       id="hero"
-      className="relative flex items-center justify-center pb-0 md:h-[90vh] h-[50vh] md:pt-[85px] pt-[60px]"
+      className="relative flex items-center justify-center pb-0 md:h-[90vh] h-[44vh] md:pt-[85px] pt-[60px]"
     >
       <div className="relative w-full h-full">
         {/* 이미지를 감쌀 컨테이너 */}
